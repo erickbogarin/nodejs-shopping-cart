cf769b94cf767a755ab0df88fb7dc11682d9e239
Erick Bogarin <erick.bogarin@gmail.com>
2016-12-07 23:21:44 -0400

    Improve the shopping cart by adding remove items feature

--- models/cart.js
+++ models/cart.js
@@ -18,6 +18,23 @@ Cart.prototype.add = function(item, id) {
   this.totalPrice += storedItem.item.price;
 }
 
+Cart.prototype.reduceByOne = function(id) {
+  this.items[id].qty--;
+  this.items[id].price -= this.items[id].item.price;
+  this.totalQty--;
+  this.totalPrice -= this.items[id].item.price;
+
+  if (this.items[id].qty <= 0) {
+    delete this.items[id];
+  }
+}
+
+Cart.prototype.removeItem = function(id) {
+  this.totalQty -= this.items[id].qty;
+  this.totalPrice -= this.items[id].price;
+  delete this.items[id];
+}
+
 Cart.prototype.generateArray = function() {
   var arr = [];
 
--- routes/index.js
+++ routes/index.js
@@ -38,6 +38,24 @@ router.get('/add-to-cart/:id', function(req, res, next) {
   });
 });
 
+router.get('/remove/:id', function(req, res, next) {
+  var productId = req.params.id;
+  var cart = new Cart(req.session.cart ? req.session.cart : { items: {} });
+
+  cart.removeItem(productId);
+  req.session.cart = cart;
+  res.redirect('/shopping-cart');
+});
+
+router.get('/reduce/:id', function(req, res, next) {
+  var productId = req.params.id;
+  var cart = new Cart(req.session.cart ? req.session.cart : { items: {} });
+
+  cart.reduceByOne(productId);
+  req.session.cart = cart;
+  res.redirect('/shopping-cart');
+});
+
 router.get('/shopping-cart', function(req, res, next) {
   var params = {};
 
